a198264638abc2ea7e4fe55d86017f1b5b72cd66
PujaGupta <puja.gupta@neosofttech.com>
2021-01-09 21:05:03 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,139 @@
+/*
+import 'react-native-gesture-handler';
+
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import HomeScreen from './database/HomeScreen';
+import NewContact from './database/NewContact';
+import UpdateUser from './database/UpdateUser';
+import ViewUser from './database/ViewUser';
+import ViewAllUser from './database/ViewAllUser';
+import DeleteUser from './database/DeleteUser';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="HomeScreen">
+        <Stack.Screen
+          name="HomeScreen"
+          component={HomeScreen}
+          options={{
+            title: 'Home', //Set Header Title
+            headerStyle: {
+              backgroundColor: '#221eeb', //Set Header color
+            },
+            headerTintColor: '#fff', //Set Header text color
+            headerTitleStyle: {
+              fontWeight: 'bold', //Set Header text style
+            },
+          }}
+        />
+        <Stack.Screen
+          name="View"
+          component={ViewUser}
+          options={{
+            title: 'View User', //Set Header Title
+            headerStyle: {
+              backgroundColor: '#221eeb', //Set Header color
+            },
+            headerTintColor: '#fff', //Set Header text color
+            headerTitleStyle: {
+              fontWeight: 'bold', //Set Header text style
+            },
+          }}
+        />
+        <Stack.Screen
+          name="ViewAll"
+          component={ViewAllUser}
+          options={{
+            title: 'View Users', //Set Header Title
+            headerStyle: {
+              backgroundColor: '#221eeb', //Set Header color
+            },
+            headerTintColor: '#fff', //Set Header text color
+            headerTitleStyle: {
+              fontWeight: 'bold', //Set Header text style
+            },
+          }}
+        />
+        <Stack.Screen
+          name="Update"
+          component={UpdateUser}
+          options={{
+            title: 'Update User', //Set Header Title
+            headerStyle: {
+              backgroundColor: '#221eeb', //Set Header color
+            },
+            headerTintColor: '#fff', //Set Header text color
+            headerTitleStyle: {
+              fontWeight: 'bold', //Set Header text style
+            },
+          }}
+        />
+        <Stack.Screen
+          name="NewContact"
+          component={NewContact}
+          options={{
+            title: 'New Contact', //Set Header Title
+            headerStyle: {
+              backgroundColor: '#221eeb', //Set Header color
+            },
+            headerTintColor: '#fff', //Set Header text color
+            headerTitleStyle: {
+              fontWeight: 'bold', //Set Header text style
+            },
+          }}
+        />
+        <Stack.Screen
+          name="Delete"
+          component={DeleteUser}
+          options={{
+            title: 'Delete User', //Set Header Title
+            headerStyle: {
+              backgroundColor: '#221eeb', //Set Header color
+            },
+            headerTintColor: '#fff', //Set Header text color
+            headerTitleStyle: {
+              fontWeight: 'bold', //Set Header text style
+            },
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
+*/
+
+//1.Components==
+
+import React from 'react';
+import {Text,StyleSheet,View} from 'react-native';
+
+const HelloWorldApp = () => {
+  return (
+    <View style = { styles.container }>
+      <Text>Hello, world!</Text>
+    </View>
+  );
+}
+export default HelloWorldApp;
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex : 1,
+    alignItems: 'center',
+    backgroundColor: 'blue',
+    justifyContent : 'center',
+  },
+ 
+});
+  
